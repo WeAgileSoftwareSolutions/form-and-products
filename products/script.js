b5bf633520b64db52faf50679854b22ede22771f
Mayur Wankhade <mayur.wankhade@weagile.net>
2022-10-20 15:46:59 +0530

    Add form and product ui

---
+++ products/script.js
@@ -0,0 +1,30 @@
+const products = [];
+let html = "";
+const productDiv = document.querySelector(".products");
+const productCount = document.querySelector(".sort-section sub");
+
+fetch("https://dummyjson.com/products")
+  .then((response) => response.json())
+  .then((data) => {
+    products.push(...data.products);
+    products.forEach((product) => {
+      html += `
+            <div class="product">
+            <div class="thumbnail">
+               <img
+                 src=${product.thumbnail}
+                 alt="product-image"
+               />
+             </div>
+             <div class="product-details">
+               <h4>${product.title}</h4>
+               <div class="rating">${product.rating} <span>&#9733;</span></div>
+               <h5>$ ${product.price}</h5>
+               <button>Add to cart</button>
+             </div>
+           </div>
+             `;
+    });
+    productCount.textContent = `(${products.length} items)`;
+    productDiv.innerHTML = html;
+  });
